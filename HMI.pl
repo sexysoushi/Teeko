99803b739a914a88852577c3157f5974ae3f7a05
sexysoushi <barbara1603@hotmail.com>
2015-06-02 19:33:23 +0200

    Game Initialisation & GAME MODE : finish

--- HMI.pl
+++ HMI.pl
@@ -1,6 +1,6 @@
 /* HMI.pl - Barbara SCHIAVI & Paul Emile BRETEGNIER - IA41 UTBM 2015*/
 
-/*------------------------------------------- BOARD ---------------------------------------------
+/*------------------------------------------- BOARD design ---------------------------------------------
 
 | A1  | A2  | A3  | A4  | A5  |
 _______________________________
@@ -12,34 +12,51 @@ _______________________________
 _______________________________
 | A21 | A22 | A23 | A24 | A25 |
 
--------------------------------------- Game Initialisation ---------------------------------------*/
+-------------------------------------- Game Initialisation & GAME MODE ---------------------------------------*/
 
-:- consult('Teeko.pl'). % inclusion du fichier teeko.pl
+:- consult('Teeko.pl'). % inclusion of file teeko.pl
+:- dynamic playerType/2. % dynamic predicat, if player is a human or ai
 
-/* Board Init, clear list, clear dynamic predicat */
+init:- writeln('TEEKO GAME - The best AI game to infinity and beyond...'),
+	writeln('Haha, welcome ! I am Jarvis, this AI Computer.'), nl.
 
 /* Choose mode - Player vs Player or Player vs AI */
-
-/* Launch game*/
-
-
-/*-------------------------------------- GAME MODE ---------------------------------------*/
-
 %chose mode pvp ou pvAI
-choseMode:- writeln('Game mode : '), writeln('1. Human vs. Human'), writeln('2. Human vs. Computer'),
+choseMode:- writeln('What would you like to do ?'),	
+		writeln('1. Play against a friend'),
+		writeln('2. Play against me'),
+        writeln('3. Quit the application'),
         get(Mode), nl, name(Choice, [Mode]), applyMode(Choice), !.
 
-applyMode(1):- assert(playerType('B', 'human')), assert(playerType('R', 'human')), writeln('The TEEKO will begin with Player VS Player mode.'), nl, !,
-        assert(firstPlayer('B')), % player black begin. Rules of Teeko.
-        launchGame.
-applyMode(2):- assert(playerType('B', 'human')), assert(playerType('R', 'ai')), writeln('The TEEKO will begin with You VS AI Computeur mode.'), nl, !,
-        nl, assert(firstPlayer('B')), % player black begin. Rules of Teeko.
-        launchGame.
-applyMode(_):- writeln('Error'), nl, choseMode.
+applyMode(1):- resetAll, assert(playerType('B', 'human')), assert(playerType('R', 'human')), writeln('Ok. Have fun together. Player black begin !'), nl, !,
+			launchGame.
+applyMode(2):- resetAll, assert(playerType('B', 'human')), assert(playerType('R', 'ai')), writeln('Hahaha, really ? Ok. You begin.'), nl, !,
+			launchGame.
+applyMode(3):- writeln('See you soon buddy !'), halt, !.
+applyMode(_):- writeln('Error input'), nl, choseMode.
+		
+% launch the game
+launchGame:- board, nl, nl, % board display
+        writeln('Ready ? 3, 2, 1... Let s go !'),
+        placement('B'). % game begin with player Black, Rules of Teeko.
+
+% resetPlayerType: reset dynamic predicat of playerType
+resetPlayerType:- playerType('B', BType), playerType('R', RType), retract(playerType('B', BType)), retract(playerType('R', RType)).
+resetPlayerType:- !.
+
+% resetPawn: reset all pawns (dynamic predicat)
+resetPawn:- findall(Pawn, positionPawn(Pawn, _), PawnList), resetPawn(PawnList).
+resetPawn([]):- !.
+resetPawn([T|R]):- unset(T), resetPawn(R), !.
+
+% resetAll
+resetAll:- resetPlayerType, resetPawn, clearboard.
 
 /*----------------------------- BOARD DISPLAY -----------------------------------------*/
 
+clearboard:- ...
 
+board:- ...
 
 
 /*----------------------------- GAME STAGE 1 : Set pawns -----------------------------------------*/
@@ -51,7 +68,7 @@ set(Player):- nextPlayer(Player, NextPlayer), winner(NextPlayer), !, haveAWinner
 % Player set a pawn, and turn to NextPlayer
 set(Player, 'human', NexPlayer):- lastPawn(Player, Pawn), addPosPawn(Pawn), set(Player), !.
 % AI set a pawn, and turn to Player
-%set(Player, 'ai', NexPlayer):- lastPawn(Player, Pawn), addPosPawn(Pawn), set(Player), !.
+%set(Player, 'ai', NexPlayer):- bestChange(Player, 3, L, M, X, Y), setOnBoard(X, Y, M), placement(NexPlayer), !.
 
 % addCoordPawn: add coordonates of each pawn
 addPosPawn(Pawn):- lastPawn(Player, Pawn), player(Player, Name), write(Name), write(', add a position such as "An." (n for number) for the pawn '), writeln(Pawn),write(' : A') read(PosPawn), checkPos(Pawn, PosPawn), not(pawnPosition(_, PosPawn)), !, setOnBoard(Pawn, PosPawn), nl.
@@ -75,7 +92,7 @@ movePawn(Player, 'human', NextPlayer):- player(Player, Name), write(Name), write
 		read(NewPosPawn), name(NewPosPawn, NewPos), consultList(Player, NewPos), movePawn(NextPlayer), !.
 
 
-%movePawn(Player, 'ai', NextPlayer):- player(Player, Name), write(Name), write(' is playing...'), aiLevel(L), bestChange('R', 3, L, Marker, X, Y), move(X, Y, Marker),
+%movePawn(Player, 'ai', NextPlayer):- player(Player, Name), write(Name), write(' ok let me see...'), bestChange('R', 3, L, Marker, X, Y), move(X, Y, Marker),
 %       show, movePawn(NextPlayer), !.
 
 /*----------------------------- END OF GAME : haveAWinner -----------------------------------------*/
