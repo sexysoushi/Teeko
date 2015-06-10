b84d22e692ee20b048252bceda5ad34c97fae0f8
sexysoushi <barbara1603@hotmail.com>
2015-06-10 23:06:53 +0200

    mise à jour

--- HMI.pl
+++ HMI.pl
@@ -13,14 +13,14 @@
 %read(-Char)
 %nl
 
--------------------------------------- Game Initialisation & GAME MODE ---------------------------------------*/
+/*-------------------------------------- Game Initialisation & GAME MODE ---------------------------------------*/
 
 :- consult('Teeko.pl'). % inclusion of file teeko.pl
 :- dynamic playerType/2. % dynamic predicat, if player is a human or ai
 :- dynamic choseAiLevel/1. % ai level mode
 
-init:- writeln('TEEKO GAME - The best AI game to infinity and beyond...'),
-	writeln('Haha, welcome ! I am Jarvis, this AI Computer.'), nl.
+init:- nl, nl, writeln('TEEKO GAME - The best AI game to infinity and beyond...'),
+	writeln('Haha, welcome ! I am Jarvis, this AI Computer.'), nl, choseMode.
 
 /* Choose mode - Player vs Player or Player vs AI */
 %chose mode pvp ou pvAI
@@ -30,27 +30,27 @@ choseMode:- writeln('What would you like to do ?'),
         writeln('3. Quit the application'),
         get(Mode), nl, name(Choice, [Mode]), applyMode(Choice), !.
 
-applyMode(1):- resetAll, assertz(playerType('B', 'human')), assertz(playerType('R', 'human')), writeln('Ok. Have fun together. Player black begin !'), nl, !,
+applyMode(1):- /*resetAll,*/ assertz(playerType('B', 'human')), assertz(playerType('R', 'human')), writeln('Ok. Have fun together. Player black begin !'), nl, !,
 			launchGame.
 applyMode(2):- resetAll, assertz(playerType('B', 'human')), assertz(playerType('R', 'ai')), writeln('Hahaha, really ? Ok.'), nl, !,
-			choseAiLevel, launchGame.
+			getAiLevel, launchGame.
 applyMode(3):- writeln('See you soon buddy !'), halt, !.
 applyMode(_):- writeln('Error input'), nl, choseMode.
 				
 % choseDifficult of AI Computer
-choseAiLevel:- writeln('I let you choose the difficulty level of my AI :'), writeln('1. EASY'), writeln('2. MEDIUM'), writeln('3. HARD'),
+getAiLevel:- writeln('I let you choose the difficulty level of my AI :'), writeln('1. EASY'), writeln('2. MEDIUM'), writeln('3. HARD'),
         get(LevelChr), nl, nl, name(Level, [LevelChr]), applyLevelChose(Level), writeln('Its your turn to begin !').
 
 % applyLevelChose: apply Level Chose for AI Computer
 applyLevelChose(1):- assert(choseAiLevel(1)).
 applyLevelChose(2):- assert(choseAiLevel(2)).
 applyLevelChose(3):- assert(choseAiLevel(3)).
-applyLevelChose(_):- writeln('Error.'), nl, choseAiLevel.
+applyLevelChose(_):- writeln('Error.'), nl, getAiLevel.
 
 % launch the game
-launchGame:- board, nl, nl, % board display
+launchGame:- /*board,*/ nl, nl, % board display
         writeln('Ready ? 3, 2, 1... Let s go !'),
-        placement('B'). % game begin with player Black, Rules of Teeko.
+        set('B'). % game begin with player Black, Rules of Teeko.
 
 % resetPlayerType: reset dynamic predicat of playerType
 resetPlayerType:- playerType('B', BType), playerType('R', RType), retract(playerType('B', BType)), retract(playerType('R', RType)).
@@ -70,46 +70,74 @@ resetAll:- resetPlayerType, resetChoseAiLevel, resetPawn, clearboard.
 
 /*----------------------------- BOARD DISPLAY -----------------------------------------*/
 
-clearboard:- ...
+%clearboard:- ...
 
-board:- ...
+%board:- ...
 
 
 /*----------------------------- GAME STAGE 1 : Set pawns -----------------------------------------*/
 
 % Set of pawns, while all pawns are not set, nextPlayer, while nobody win
-set(Player):- not(stage2), nextPlayer(Player, NextPlayer), not(winner(NextPlayer)), !, playerType(Player, Type) /*playerType sera rentré lors du choix de la partie*/, set(Player, Type, NextPlayer).
+set(Player):- write('set1'), stageTwo/*, nextPlayer(Player, NextPlayer), not(winner(NextPlayer)), !, playerType(Player, Type), set(Player, Type, NextPlayer)*/.
 % If at the end of the stage 1 we have a winner -> end of the game
 set(Player):- nextPlayer(Player, NextPlayer), winner(NextPlayer), !, haveAWinner(NextPlayer, Type).
+
+set(Player):- move(Player).
+
 % Player set a pawn, and turn to NextPlayer
 set(Player, 'human', NexPlayer):- lastPawn(Player, Pawn), addPosPawn(Pawn), set(Player), !.
+/*
 % AI set a pawn, and turn to Player
 set(Player, 'ai', NexPlayer):- write('Its my turn... '), choseAiLevel(L), bestChange(Player, 3, L, Pawn, A), setOnBoard(Pawn, A), placement(NexPlayer), !.
-
+*/
 % addCoordPawn: add coordonates of each pawn
-addPosPawn(Pawn):- lastPawn(Player, Pawn), player(Player, Name), write(Name), write(', add a position such as "An." (n for number) for the pawn '), writeln(Pawn),write(' : A') read(PosPawn), checkPos(Pawn, PosPawn), not(pawnPosition(_, PosPawn)), !, setOnBoard(Pawn, PosPawn), nl.
+addPosPawn(Pawn):- lastPawn(Player, Pawn), player(Player, Name), write(Name), write(', add a position such as "An." (n for number) for the pawn '),
+		write(Pawn), write(' : A'), read(PosPawn), checkPos(Pawn, PosPawn), not(pawnPosition(_, PosPawn)), !, setOnBoard(Pawn, PosPawn), nl.
 addPosPawn(Pawn):- nl, writeln(' Unavailable position. Try again !'), addCoordPawn(Pawn).
 
 % checkPos: check position add by the player
-checkPos(_, N):- between()1, 25, N), !. % check position add by the player
+checkPos(_, N):- between(1, 25, N), !. % check position add by the player
 checkPos(Pawn, N):- write('Error position. Try again ! (dont forget the . )'), addPosPawn(Pawn). % else return to addPosPawn
 
-% stage2: predicat allows stage 2 if all pawns are set on board.
-stage2:- not(lastPawn('B', _)), not(lastPawn('R', _)), !, not(winner('B')), not(winner('R')).
+% stageTwo: predicat allows stage 2 if all pawns are set on board.
+stageTwo:- write('stageTwo'), not(lastPawn('B', _)), not(lastPawn('R', _)), !, not(winner('B')), not(winner('R')).
 
 /*----------------------------- GAME STAGE 2 : Move pawns -----------------------------------------*/
 
 % movePawn: select a pawn to move and move it
-movePawn(Player):- nextPlayer(Player, NextPlayer), not(winner(NextPlayer)), !, playerType(Player, Type), movePawn(Player, Type, NextPlayer).
+move(Player):- nextPlayer(Player, NextPlayer), not(winner(NextPlayer)), !, playerType(Player, Type), move(Player, Type, NextPlayer).
 % If at the end of the stage 2 we have a winner -> end of the game
-movePawn(Player):- nextPlayer(Player, NextPlayer), winner(NextPlayer), !, haveAWinner(NextPlayer, Type).
-/*
+move(Player):- nextPlayer(Player, NextPlayer), winner(NextPlayer), !, haveAWinner(NextPlayer, Type).
 % Player move a pawn, and turn to NextPlayer
-movePawn(Player, 'human', NextPlayer):- player(Player, Name), write(Name), writeln(', chose a pawn as "2." : '), read(PawnToMove), writeln(', and add a position such as "An." (n for number) : A'), 
-		read(NewPosPawn), name(NewPosPawn, NewPos), consultList(Player, NewPos), movePawn(NextPlayer), !.
+move(Player, 'human', NextPlayer):- movePawn(Player), move(NextPlayer), !. /*consultList(Player, NewPos)*/
 
-movePawn(Player, 'ai', NextPlayer):- writeln('Its my turn, ok let me see...'), choseAiLevel(L), bestChange('R', 3, L, Pawn, A), moveOnBoard(Pawn, A), movePawn(NextPlayer), !.
+/*
+move(Player, 'ai', NextPlayer):- writeln('Its my turn, ok let me see...'), choseAiLevel(L), bestChange('R', 3, L, Pawn, A), moveOnBoard(Pawn, A), movePawn(NextPlayer), !.
 */
+% movePawn
+movePawn(Player):- player(Player, Name), write(Name), writeln('. Chose a pawn as "2." (e.g. for your P2) : '), read(PawnToMove), name(PawnToMove, PawnTo),
+					writeln(', and add a position such as "An." (n for number) : A'), read(NewPosPawn), name(NewPosPawn, NewPos), checkPawn(Player, PawnTo, NewPos).
+					
+% checkPawn: check correct pawn add by the player
+checkPawn(Player, PawnTo, NewPos):- between(1, 4, PawnTo), between(1, 25, NewPos),!, selectPawn(Player, PawnTo, Pawn), checkPossibleMove(Pawn, NewPos). % check position and pawn add by the player
+checkPawn(Player, _, _):- write('Error Pawn or new position request. Try again ! (dont forget the . )'), movePawn(Player). % else return to addPosPawn
+
+%selectPawn : return the Pawn of the player at the position PawnTo on the PawnList
+selectPawn(Player, PawnTo, Pawn):- pawnList(Player, PawnList), selectPawn(Player,  PawnTo, Pawn, 1, PawnList).
+selectPawn(Player, PawnTo, Pawn, N, [T|R]):- N \= PawnTo, !, N1 is N+1, selectPawn(Player, PawnTo, Pawn, N1, R).
+selectPawn(Player, PawnTo, Pawn, N, [T|R]):- N == PawnTo, Pawn is T.
+
+% checkPossibleMove : check if its a correct move of the pawn and if the case is free.
+checkPossibleMove(Pawn, ToA):- pawnPosition(Pawn, FromA), NewA is FromA - 6, ToA == NewA, !, calcSet(Pawn, ToA), moveOnBoard(Pawn, ToA).
+checkPossibleMove(Pawn, ToA):- pawnPosition(Pawn, FromA), NewA is FromA - 5, ToA == NewA, !, calcSet(Pawn, ToA), moveOnBoard(Pawn, ToA).
+checkPossibleMove(Pawn, ToA):- pawnPosition(Pawn, FromA), NewA is FromA - 4, ToA == NewA, !, calcSet(Pawn, ToA), moveOnBoard(Pawn, ToA).
+checkPossibleMove(Pawn, ToA):- pawnPosition(Pawn, FromA), NewA is FromA - 1, ToA == NewA, !, calcSet(Pawn, ToA), moveOnBoard(Pawn, ToA).
+checkPossibleMove(Pawn, ToA):- pawnPosition(Pawn, FromA), NewA is FromA + 1, ToA == NewA, !, calcSet(Pawn, ToA), moveOnBoard(Pawn, ToA).
+checkPossibleMove(Pawn, ToA):- pawnPosition(Pawn, FromA), NewA is FromA + 4, ToA == NewA, !, calcSet(Pawn, ToA), moveOnBoard(Pawn, ToA).
+checkPossibleMove(Pawn, ToA):- pawnPosition(Pawn, FromA), NewA is FromA + 5, ToA == NewA, !, calcSet(Pawn, ToA), moveOnBoard(Pawn, ToA).
+checkPossibleMove(Pawn, ToA):- pawnPosition(Pawn, FromA), NewA is FromA + 6, ToA == NewA, !, calcSet(Pawn, ToA), moveOnBoard(Pawn, ToA).
+
+
 /*----------------------------- END OF GAME : haveAWinner -----------------------------------------*/
 
 haveAWinner(Player, 'human'):- writeln(' We have a winner !!'), player(Player, Name), write(Name), writeln(' won this game !').
--- Teeko.pl
+++ Teeko.pl
@@ -1,5 +1,6 @@
 /* Teeko.pl - Barbara SCHIAVI & Paul Emile BRETEGNIER - IA41 UTBM 2015*/
 :- consult('AI.pl'). % inclusion of file AI.pl
+:- dynamic vars/1.
 
 /* Prolog function use */
 %between(+Low, +High, ?Value)
@@ -35,17 +36,15 @@ Pos + 6
  -------------------------------------------------------------------------------------------------------*/
  
  /* Board def */
-Vars = 	[
-		A1 , A2,  A3 , A4 , A5 ,
+vars([A1, A2, A3, A4, A5,
 		A6 , A7 , A8 , A9 , A10,
 		A11, A12, A13, A14, A15,
 		A16, A17, A18, A19, A20,
-		A21, A22, A23, A24, A25}
-		]
+		A21, A22, A23, A24, A25
+		]).
 		
 /* Win Line */
-Line =  [
-		[A1, A2, A3, A4],
+rowList([[A1, A2, A3, A4],
 		[A2, A3, A4 ,A5],
 		[A6, A7, A8, A9],
 		[A7, A8, A9, A10],
@@ -55,11 +54,10 @@ Line =  [
 		[A17, A18, A19, A20],
 		[A21, A22, A23, A24],
 		[A22, A23, A24, A25]
-		]
+		]).
 
 /* Win Column */
-Column = [
-		[A1, A6, A11, A16],
+columnList([[A1, A6, A11, A16],
 		[A6, A11, A16, A21],
 		[A2, A7, A12, A17],
 		[A7, A12, A17, A22],
@@ -69,11 +67,10 @@ Column = [
 		[A9, A14, A19, A24],
 		[A5, A10, A15, A20],
 		[A10, A15, A20, A25]
-		]
+		]).
 		
 /* Win Diago */
-Diago = [
-		[A1, A7, A13, A19],
+diagoList([[A1, A7, A13, A19],
 		[A7, A13, A19, A25],
 		[A2, A8, A14, A20],
 		[A6, A12, A18, A24],
@@ -81,11 +78,10 @@ Diago = [
 		[A5, A9, A13, A17],
 		[A9, A13, A17, A21],
 		[A10, A14, A18, A22]
-		]
+		]).
 		
 /* Win Square */		
-Square= [
-		[A1, A2, A6, A7],
+squareList([[A1, A2, A6, A7],
 		[A2, A3, A7, A8],
 		[A3, A4, A8, A9],
 		[A4, A5, A9, A10],
@@ -101,10 +97,10 @@ Square= [
 		[A14, A15, A19, A20],
 		
 		[A16, A17, A21, A22],
-		[A17, A18, A22, A23];
+		[A17, A18, A22, A23],
 		[A18, A19, A23, A24],
 		[A19, A20, A24, A25]
-		]
+		]).
 		
 
 /*--------------------------------------------- Player vs Player ------------------------------------------------------*/
@@ -133,64 +129,64 @@ pawnList('B', ['B1', 'B2', 'B3', 'B4']).
 pawnList('R', ['R1', 'R2', 'R3', 'R4']).
 
 %Return the next player who will play. predicat nextPlayer(+X, -Y), X and Y elements.
-nextPlayer(X, Y):- player(Y, _), not(Y = X).
+nextPlayer(X, Y):- write('nextPlayer'), player(Y, _), not(Y = X).
 
 %Return if combo of pawn is win combo
-winner('B') :-pawnPosition('B1', BA1), pawnPosition('B2', BA2), pawnPosition('B3', BA3), pawnPosition('B4', BA4), winner(BA1, BA2, BA3, BA4), !.
-winner('R') :-pawnPosition('R1', RA1), pawnPosition('R2', RA2), pawnPosition('R3', RA3), pawnPosition('B4', RA4), winner(RA1, RA2, RA3, RA4), !.
-% by raw
+winner('B'):- write('winnerB'),pawnPosition('B1', BA1), pawnPosition('B2', BA2), pawnPosition('B3', BA3), pawnPosition('B4', BA4), winner(BA1, BA2, BA3, BA4), !.
+winner('R'):- write('winnerR'),pawnPosition('R1', RA1), pawnPosition('R2', RA2), pawnPosition('R3', RA3), pawnPosition('B4', RA4), winner(RA1, RA2, RA3, RA4), !.
+% by row
 winner(A1, A2, A3, A4):- permutation([A1, A2, A3, A4], [PA1, PA2, PA3, PA4]), checkBetween([PA1, PA2, PA3, PA4]), neighborRaw(L, [PA1, PA2, PA3, PA4]), !.
 % by column
 winner(A1, A2, A3, A4):- permutation([A1, A2, A3, A4], [PA1, PA2, PA3, PA4]), checkBetween([PA1, PA2, PA3, PA4]), neighborColumn(L, [PA1, PA2, PA3, PA4]), !.
 % by diagnal up
-winner(A1, A2, A3, A4):- permutation([A1, A2, A3, A4], [PA1, PA2, PA3, PA4]), checkBetween([PA1, PA2, PA3, PA4]), neighborDiagonalUp(L, [PA1, PA2, PA3, PA4]), !.
+winner(A1, A2, A3, A4):- permutation([A1, A2, A3, A4], [PA1, PA2, PA3, PA4]), checkBetween([PA1, PA2, PA3, PA4]), neighborDiagonal(L, [PA1, PA2, PA3, PA4]), !.
 % by diagonal down
-winner(A1, A2, A3, A4):- permutation([A1, A2, A3, A4], [PA1, PA2, PA3, PA4]), checkBetween([PA1, PA2, PA3, PA4]), neighborDiagonalDown(L, [PA1, PA2, PA3, PA4]), !.
+winner(A1, A2, A3, A4):- permutation([A1, A2, A3, A4], [PA1, PA2, PA3, PA4]), checkBetween([PA1, PA2, PA3, PA4]), neighborSquare(L, [PA1, PA2, PA3, PA4]), !.
 
-%Check if pawns are on a same win raw
-neighborRaw().
-neighborRaw([L|R1], [T|R2]):- rawList([L|R1]), member(T, L), !, allInList(L, R2).
-neighborRaw([L|R1], [T|R2]):- rawList([L|R1]), neighborRaw(R1, T).
+%Check if pawns are on a same win row
+neighborRaw([], []).
+neighborRaw([L|R1], [T|R2]):- rowList([L|R1]), member(T, L), !, allInList(L, R2).
+neighborRaw([L|R1], [T|R2]):- rowList([L|R1]), neighborRaw(R1, T).
 
 %Check if pawns are on a same win column
-neighborColumn().
+neighborColumn([], []).
 neighborColumn([L|R1], [T|R2]):- columnList([L|R1]), member(T, L), !, allInList(L, R2).
 neighborColumn([L|R1], [T|R2]):- columnList([L|R1]), neighborColumn(R1, T).
 
 %Check if pawns are on a same win diagonal up
-neighborDiagonalUp().
-neighborDiagonalUp([L|R1], [T|R2]):- diagUpList([L|R1]), member(T, L), !, allInList(L, R2).
-neighborDiagonalUp([L|R1], [T|R2]):- diagUpList([L|R1]), neighborDiagonalUp(R1, T).
+neighborDiagonalUp([], []).
+neighborDiagonalUp([L|R1], [T|R2]):- diagoList([L|R1]), member(T, L), !, allInList(L, R2).
+neighborDiagonalUp([L|R1], [T|R2]):- diagoList([L|R1]), neighborDiagonal(R1, T).
 
 %Check if pawns are on a same win diagonal down
-neighborDiagonalDown().
-neighborDiagonalDown([L|R1], [T|R2]):- diagDownList([L|R1]), member(T, L), !, allInList(L, R2).
-neighborDiagonalDown([L|R1], [T|R2]):- diagDownList([L|R1]), neighborDiagonalDown(R1, T).
+neighborDiagonalDown([], []).
+neighborDiagonalDown([L|R1], [T|R2]):- squareList([L|R1]), member(T, L), !, allInList(L, R2).
+neighborDiagonalDown([L|R1], [T|R2]):- squareList([L|R1]), neighborSquare(R1, T).
 
 %Check all pawn in a list
 allInList(L, [T|R]):- member(T, L), allInList(L, R).
 
 %Return last element of PawnList
-lastPawn(Player, Pawn) :- pawn(Player, PawnList), haveNoPos(PawnList, Pawn).
+lastPawn(Player, Pawn) :- pawnList(Player, PawnList), haveNoPos(PawnList, Pawn).
 
 %Return the pawn of pawnlist haven't got yet position
-haveNoPos([P|_], P) :- not(pawnPosition(P,_,_)), !.
-haveNoPos([T|R], P) :- pawnPosition(T,_,_), haveNoPos(R, P).
+haveNoPos([P|_], P) :- not(pawnPosition(P,_)), !.
+haveNoPos([T|R], P) :- pawnPosition(T,_), haveNoPos(R, P).
 
-%Set marker on board
+%Set Pawn on board
 setOnBoard(Pawn, A) :- nonvar(Pawn), nonvar(A), calcSet(Pawn, A), assertz(pawnPosition(Pawn, A)), !.
 
-%Unset marker off board
+%Unset Pawn off board
 unsetOffBoard(Pawn) :- nonvar(Pawn), pawnPosition(Pawn, A), retract(pawnPosition(Pawn, A)), !.
 
 % if position is available
 calcSet(Pawn, A) :- between(1, 25, A), not(pawnPosition(_, A)), not(pawnPosition(Pawn, _)).
 
-%calcAllSet : calculate all positions available to set a marker and check if the marker is the first which can be set
+%calcAllSet : calculate all positions available to set a Pawn and check if the Pawn is the first which can be set
 calcAllSet(Player, Pawn, A) :- lastPawn(Player, Pawn), calcSet('NE', Pawn).
 
-%moveOnBoard : Move pawn to nex position and retract the last position
-moveOnBoard(Pawn, A) :- pawnPosition(Pawn, FromA), calcMove(Pawn, ToA), assert(pawnPosition(Pawn, ToA)), retract(pawnPosition(Pawn, FromA)), !.
+%moveOnBoard : Move pawn to next position and retract the last position
+moveOnBoard(Pawn, ToA) :- pawnPosition(Pawn, FromA), assert(pawnPosition(Pawn, ToA)), retract(pawnPosition(Pawn, FromA)), !.
 
 %calcAllMove : calculate all moves available for all pawns
 calcAllMove(Player, Pawn, ToA) :- pawnList(Player, PawnList), member(Pawn, PawnList), calcMove(Pawn, ToA).
@@ -199,7 +195,6 @@ calcAllMove(Player, Pawn, ToA) :- pawnList(Player, PawnList), member(Pawn, PawnL
 calcMove(Pawn, ToA):- pawnPosition(Pawn, FromA),
 				between(1, 25, ToA),
 				between(4, 6, Val)
-
 
 inRange([TOX, TOY], 1, 5),
     MAXX is FROMX + 1, MINX is FROMX - 1, inRange([TOX], MINX, MAXX),
